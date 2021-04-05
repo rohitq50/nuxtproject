eb673a59ac9d0578cb3f8b10ed06adb5bfdd04d9
Rohit Kumar <rohitq50@gmail.com>
2021-04-05 11:02:23 +0530

    some new changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,8 @@
 <template>
-	<h1>Hello world!</h1>
+	<main>
+		<h1>Hello world!</h1>
+		<NuxtLink to="/about">
+			About (internal link that belongs to the Nuxt App)
+		</NuxtLink>
+	</main>
 </template>
